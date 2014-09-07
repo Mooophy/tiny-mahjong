687316336c8c9371850ab74bef8605c375f29d61
mooophy <MoophyAlan@gmail.com>
2014-09-07 14:46:03 +1200

    add testing code 	modified:   mahjong/game_master.h

--- mahjong/game_master.h
+++ mahjong/game_master.h
@@ -72,3 +72,18 @@ private:
 }//namespace
 
 #endif // GAME_MASTER_H
+
+//! @test
+//!
+//#include <iostream>
+//#include "game_master.h"
+//int main()
+//{
+//    mj::GameMaster gm;
+//    for(auto i = 0; i != 136; ++i)
+//        gm.license().print() << "\n";
+
+//    std::cout << "exit normally\n";
+//    return 0;
+//}
+

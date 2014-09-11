cfa621f64d45a83b2b46bc980d65500ddaa76db3
mooophy <MoophyAlan@gmail.com>
2014-09-11 12:40:40 +1200

    test box with sequence 	modified:   mahjong/main.cpp

--- mahjong/main.cpp
+++ mahjong/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-#include <tile.hpp>
+#include "tile.hpp"
+#include "random_sequence.hpp"
+#include <deque>
 
 int main()
 {
     auto box = mj::build_box<std::vector<mj::Tile>>();
-    for(const auto& tile : box)
-        tile.print() << "\n";
+    auto sequence = mj::generate_random_sequence<std::deque<unsigned>>();
+
+    for(unsigned u = 0; u != box.size(); ++u)
+    {
+        box[sequence.back()].print() << " u=" << u << "\n";
+        sequence.pop_back();
+    }
 
     std::cout << "exit normally\n";
     return 0;
